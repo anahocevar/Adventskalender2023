f86f5f6271d79d50431d0db3f5377044c2fd9fb5
anahocevar <ahocevara@gmail.com>
2023-12-02 09:28:57 +0100

    12/2

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ import streamlit as st
 
 def app():
     # UPDATE index every day
-    index = 0
+    index = 1
     day_picked = str(index+1) + " December"
 
     # UPDATE days dict every day
-    days = {"1 December": "https://media.tenor.com/SX4QflcMDKsAAAAd/baby-snow.gif"}
+    days = {"1 December": "https://media.tenor.com/SX4QflcMDKsAAAAd/baby-snow.gif",
+            "2 December": "https://64.media.tumblr.com/2a6a2c0be28ab9b42f83c9ce45cb69a3/6d71e206c2329a31-77/s250x400/b5c7d4d5bddb0c733f65ef06897d652728ea1609.gif"}
 
     # UPDATE the tuple of days in the radio every day
     with st.sidebar:
         day_picked = st.radio(
           "",
-            ("1 December",), index=0
+            ("1 December", "2 December"), index=index
     )    
-
+           
     st.title(day_picked)
     st.image(
             days[day_picked],
