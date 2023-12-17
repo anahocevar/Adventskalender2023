d6cb386e4cd9c6d941bddb99b8f0f4f55dd9d64c
anahocevar <ahocevara@gmail.com>
2023-12-18 00:10:40 +0100

    12/18

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ def app():
             "14 December": "https://i.gifer.com/OuBq.gif",
             "15 December": "https://i.pinimg.com/originals/28/94/50/2894501b7551293d410858b646cdccba.gif",
             "16 December": "https://media1.giphy.com/media/ZdlSqZuzzMB1JT3nwT/giphy.gif",
-            "17 December": "https://i.chzbgr.com/full/8474428672/h87705AF7/dachshund-winter-fun"}
+            "17 December": "https://i.chzbgr.com/full/8474428672/h87705AF7/dachshund-winter-fun",
+            "18 December": "https://media2.giphy.com/media/133c9SJrrC2bni/giphy.gif"}
 
     index = len(days) - 1
     day_picked = str(index+1) + " December"
